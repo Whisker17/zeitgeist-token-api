3017695010bc278dd68e0f60e4c635bbb30dbbdd
Whisker17 <yukaijun123456@163.com>
2022-07-28 12:26:04 +0800

    update

---
+++ src/controllers/stakingController.ts
@@ -0,0 +1,78 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { injectable, inject } from 'inversify';
+import { ContainerTypes } from '../containertypes';
+import { IDappsStakingService } from '../services/stakingService';
+import { IStatsIndexerService, PeriodType } from '../services/statsIndexer';
+import { ControllerBase } from './controllerBase';
+import { IControllerBase } from './iControllerBase';
+
+@injectable()
+export class StakingController extends ControllerBase implements IControllerBase {
+    constructor(
+        @inject(ContainerTypes.DappsStakingService) private _stakingService: IDappsStakingService,
+        @inject(ContainerTypes.StatsIndexerService) private _indexerService: IStatsIndexerService,
+    ) {
+        super();
+    }
+
+    public register(app: express.Application): void {
+        /**
+         * @description staking APR route
+         */
+        app.route('/api/v1/staking/apr').get(async (req: Request, res: Response) => {
+            /*
+                #swagger.description = 'Query APR'
+            */
+            try {
+                res.json(await this._stakingService.calculateApr());
+            } catch (err) {
+                this.handleError(res, err as Error);
+            }
+        });
+
+        /**
+         * @description staking APY route
+         */
+        app.route('/api/v1/staking/apy').get(async (req: Request, res: Response) => {
+            /*
+                #swagger.description = 'Query APY'
+            */
+            try {
+                res.json(await this._stakingService.calculateApy());
+            } catch (err) {
+                this.handleError(res, err as Error);
+            }
+        });
+
+        /**
+         * @description staking TVL route v1.
+         */
+        app.route('/api/v1/dapps-staking/tvl/:period').get(async (req: Request, res: Response) => {
+            /*
+                #swagger.description = 'Query TVL in a specific period'
+                #swagger.parameters['period'] = {
+                    in: 'path',
+                    description: 'The period type. Supported values: 7 days 30 days, 90 days, 1 year',
+                    required: true,
+                }
+            */
+            res.json(await this._indexerService.getDappStakingTvl(req.params.period as PeriodType));
+        });
+
+        /**
+         * @description staking rewards route v1.
+         */
+        app.route('/api/v1/dapps-staking/earned/:address').get(async (req: Request, res: Response) => {
+            /*
+                #swagger.description = 'Query staking rewards for a specific address'
+                #swagger.parameters['address'] = {
+                    in: 'path',
+                    description: 'Wallet address. Supported address format: SS58',
+                    required: true,
+                }
+            */
+            res.json(await this._stakingService.getEarned(req.params.address as string));
+        });
+    }
+}
---
+++ src/services/stakingService.ts
@@ -0,0 +1,103 @@
+import { injectable, inject } from 'inversify';
+import { aprToApy } from 'apr-tools';
+import { IApiFactory } from '../client/apiFactory';
+import { AprCalculationData } from '../models/AprCalculationData';
+import { networks } from '../const';
+import { defaultAmountWithDecimals, getSubscanOption } from '../utils';
+import axios from 'axios';
+import { IZeitgeistApi, Transaction } from '../client/baseApi';
+import { ContainerTypes } from '../containertypes';
+
+export interface IStakingService {
+    calculateApr(): Promise<number>;
+    calculateApy(): Promise<number>;
+    getEarned(address?: string): Promise<number>;
+}
+
+// Ref: https://github.com/PlasmNetwork/Astar/blob/5b01ef3c2ca608126601c1bd04270ed08ece69c4/runtime/shiden/src/lib.rs#L435
+// Memo: 50% of block rewards goes to dappsStaking, 50% goes to block validator
+const DAPPS_REWARD_RATE = 0.5;
+
+const TS_FIRST_BLOCK = {
+    [networks.zeitgeist.name]: 1642041546, //  Ref: 2022-01-13 02:39:06  https://zeitgeist.subscan.io/block/1
+};
+
+@injectable()
+/**
+ * staking calculation service.
+ */
+export class StakingService implements IStakingService {
+    constructor(@inject(ContainerTypes.ApiFactory) private _apiFactory: IApiFactory) {}
+
+    public async calculateApr(): Promise<number> {
+        try {
+            const api = this._apiFactory.getApiInstance();
+            const data = await api.getAprCalculationData();
+            const decimals = await api.getChainDecimals();
+
+            const blockRewards = Number(defaultAmountWithDecimals(data.blockRewards, decimals));
+            const averageBlocksPerMinute = this.getAverageBlocksPerMins(data);
+            const averageBlocksPerDay = averageBlocksPerMinute * 60 * 24;
+            const dailyEraRate = averageBlocksPerDay / data.blockPerEra.toNumber();
+            const eraRewards = data.blockPerEra.toNumber() * blockRewards;
+            const annualRewards = eraRewards * dailyEraRate * 365.25;
+
+            const tvl = await api.getTvl();
+            const totalStaked = Number(ethers.utils.formatUnits(tvl.toString(), decimals));
+            const stakerBlockReward = (1 - data.developerRewardPercentage) * DAPPS_REWARD_RATE;
+            const stakerApr = (annualRewards / totalStaked) * stakerBlockReward * 100;
+
+            return stakerApr;
+        } catch (e) {
+            console.error(e);
+            throw new Error(
+                'Unable to calculate network APR. Most likely there is an error fetching data from a node.',
+            );
+        }
+    }
+
+    public async calculateApy(): Promise<number> {
+        try {
+            const apr = await this.calculateApr();
+            return aprToApy(apr);
+        } catch {
+            throw new Error(
+                'Unable to calculate network APY. Most likely there is an error fetching data from a node.',
+            );
+        }
+    }
+
+    private getAverageBlocksPerMins(chainId: string, data: AprCalculationData): number {
+        const currentTs = Math.floor(data.timeStamp.toNumber() / 1000);
+        const minsChainRunning = (currentTs - TS_FIRST_BLOCK[chainId]) / 60;
+        const avgBlocksPerMin = data.latestBlock.toNumber() / minsChainRunning;
+
+        return avgBlocksPerMin;
+    }
+
+    public async getEarned(address: string): Promise<number> {
+        try {
+            // Docs: https://support.subscan.io/#staking-api
+            const url = networks.zeitgeist.subscanUrl + '/api/scan/staking_history';
+            const option = getSubscanOption();
+
+            const body = {
+                row: 20,
+                page: 0,
+                address,
+            };
+
+            const result = await axios.post(url, body, option);
+
+            if (result.data) {
+                const earned = result.data.data.sum;
+                return Number(ethers.utils.formatEther(earned));
+            } else {
+                return 0;
+            }
+        } catch (e) {
+            console.error(e);
+            throw new Error('Something went wrong. Most likely there is an error fetching data from Subscan API.');
+        }
+    }
+}
